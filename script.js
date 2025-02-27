1b27c7cfc95ec8fb76a128d49ec7b7fe759f1ef4
Konrad <jokerrooss@gmail.com>
2025-02-27 22:31:17 +0100

    changes

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+    function(position){
+        console.log(position);
+        const {latitude} = position.coords
+        const {longitude} = position.coords
+
+        const coords = [latitude, longitude]
+
+
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        
+
+        map.on('click', function(mapEvent){
+            
+            form.classList.remove('hidden')
+            
+            // console.log(mapEvent);
+            // const {lat, lng} = mapEvent.latlng
+            // L.marker([lat, lng]).addTo(map)
+            // .bindPopup(L.popup({
+            //     maxWidth: 250,
+            //     minWidth: 100,
+            //     autoClose: false,
+            //     closeOnClick: false,
+            //     className: 'running-popup' 
+            // })).setPopupContent('Workout')
+            // .openPopup();
+        })
+    }, function(){
+        alert('Could not get your position')
+    })
